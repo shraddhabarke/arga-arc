62a68b693b64c7d89c4b1314015836a0fc65ce90
Shraddha Barke <sbarke@eng.ucsd.edu>
2023-10-13 01:32:15 -0700

    compute subset of nodes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from transform import *
 from filter import *
 
 if __name__ == "__main__":
-    taskNumber = "4258a5f9"
+    taskNumber = "bb43febb"
     task = Task("dataset/" + taskNumber + ".json")
     task.abstraction = "nbccg"
     move_node_instance = MoveNode(Direction.LEFT)    
@@ -35,6 +35,13 @@ if __name__ == "__main__":
                                                               input in task.train_input]
     task.output_abstracted_graphs_original[task.abstraction] = [getattr(output, Image.abstraction_ops[task.abstraction])() for
                                                                output in task.train_output]
+    for graph in task.input_abstracted_graphs_original[task.abstraction][0].graph.nodes(data=True):
+        print("this one:", graph[0], graph[1])
+
+    # Example usage:
+    subsets = task.input_abstracted_graphs_original[task.abstraction][0].get_all_subsets()
+    for subset in subsets:
+        print("subset", subset)
     task.get_static_inserted_objects()
     task.get_static_object_attributes(task.abstraction)
     filtered_nodes = []
@@ -56,6 +63,8 @@ if __name__ == "__main__":
         for a, e in zip(actual.graph.nodes(data=True), expected.graph.nodes(data=True)):
             print("actual:", a[0], a[1])
             print("expected:", e[0], e[1])
+
+    # Testing a sequence of transforms and filters
 
     transforms_list = [UpdateColor(Color.C3), MoveNode(Direction.UP)]
     trans_sequence = Transforms(transforms_list)
