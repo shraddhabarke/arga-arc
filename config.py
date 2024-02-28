c58de1b2141a8944d1cf905c3fe0a3f850b307ee
Saketh Kasibatla <skasibatla@ucsd.edu>
2024-02-28 21:44:09 +0000

    read openai key from .env

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 import typing as t
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DIR_CONTAINING_FILE = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,6 +12,8 @@ DIR_CONTAINING_FILE = os.path.dirname(os.path.abspath(__file__))
 @dataclass
 class Config:
     ROOT_DIR = Path(DIR_CONTAINING_FILE)
+    OPENAI_SECRET_KEY: str = os.getenv("OPENAI_SECRET_KEY")
+    OPENAI_ORGANIZATION: str = os.getenv("OPENAI_ORGANIZATION")
 
 
 CONFIG = Config()
